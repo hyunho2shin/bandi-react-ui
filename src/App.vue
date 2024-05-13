a5fbfab67de99591d89a4004b0ab31b0f3e44d93
shinhyunho <youarefr@gmail.com>
2024-05-13 13:36:02 +0900

    .

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import SinkSse from './components/SinkSse.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+<!--    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+<!--      <HelloWorld msg="You did it!" />-->
+      <SinkSse />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+<!--        <RouterLink to="/">Home</RouterLink>-->
+<!--        <RouterLink to="/about">About</RouterLink>-->
       </nav>
     </div>
   </header>
 
-  <RouterView />
+<!--  <RouterView />-->
 </template>
+
+
+
 
 <style scoped>
 header {
---
+++ src/components/SinkSse.vue
@@ -0,0 +1,19 @@
+<!--<script setup lang="ts">-->
+<script>
+
+import { onMounted } from 'vue';
+
+export default {
+  setup() {
+    onMounted(() => {
+      console.log('Component created');
+      let es = new EventSource(`http://localhost:8888/bandi/events`);
+      es.onmessage = function(e) {
+        console.log(e.data);
+      };
+
+    });
+  }
+}
+
+</script>
